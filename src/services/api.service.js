26fa8adc3031c91803bd7800da42185398648715
Roman <roma00062@gmail.com>
2022-02-13 17:14:41 +0100

    Created get request for Firebase

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -9,12 +9,27 @@ class ApiService {
                 method: 'post',
                 body: JSON.stringify(post)
             })
-            const response = await fetch(request);
-            return await response.json()
+            return useRequest(request)
         } catch(err) {
             console.error(err); 
         }
     }
+
+    async fetchPosts() {
+        try {
+            const request = new Request(`${this.url}/posts.json`, {
+                method: 'get'
+            })
+            return useRequest(request)
+        } catch(err) {
+            console.error(err);
+        }
+    }
+}
+
+async function useRequest(request) {
+    const response = await fetch(request);
+    return await response.json()
 }
 
 export const apiService = new ApiService('https://project-blog-49723-default-rtdb.europe-west1.firebasedatabase.app')
