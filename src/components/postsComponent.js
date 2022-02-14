fddebdf5856c4ff1a717e3e8f87e7147a8f8d789
Roman <roma00062@gmail.com>
2022-02-14 21:50:16 +0100

    Created feature save to favorite in localStorage

--- src/components/postsComponent.js
+++ src/components/postsComponent.js
@@ -8,6 +8,10 @@ export class PostsComponent extends Component {
         this.loader = loader;
     }
 
+    init() {
+        this.$el.addEventListener('click', buttonHandler.bind(this))
+    }
+
     async onShow() {
         this.loader.show();
         const fbData = await apiService.fetchPosts();
@@ -29,7 +33,9 @@ function renderPost(post) {
         ? '<li class="tag tag-blue tag-rounded">Новость</li>'
         : '<li class="tag  tag-rounded">Заметка</li>';
     
-    const button = '<button class="button-round button-small button-primary">Сохранить</button>';
+    const button = (JSON.parse(localStorage.getItem('favorites')) || []).includes(post.id)
+    ?`<button class="button-round button-small button-danger" data-id="${post.id}">Удалить</button>`
+    : `<button class="button-round button-small button-primary" data-id="${post.id}">Сохранить</button>`
 
     return `
         <div class="panel">
@@ -48,4 +54,46 @@ function renderPost(post) {
         </div>
     </div>
     `
+}
+
+function buttonHandler(e) {
+   const $el = e.target;
+   const id = $el.dataset.id;
+   const title = $el.title;
+
+
+   if(id) {
+      let favorites = JSON.parse(localStorage.getItem('favorites')) || [] //if null it will be empty []
+
+      if (favorites.includes(id)) {
+          //delete el
+        $el.textContent = 'Сохранить';
+        $el.classList.add('button-primary');
+        $el.classList.remove('button-danger');
+        favorites = favorites.filter(fId => fId != id);
+      } else {
+          //add el
+        $el.textContent = 'Удалить';
+        $el.classList.remove('button-primary');
+        $el.classList.add('button-danger');
+        favorites.push(id);
+      }
+
+      localStorage.setItem("favorites", JSON.stringify(favorites))
+   }
+
+   if(title) {
+    let postName = JSON.parse(localStorage.getItem('postName')) || []
+
+    if (postName.includes(title)) {
+        //delete el
+      postName = postName.filter(fName => fName != title)
+    } else {
+        //add el
+      postName.push(title);
+
+    }
+
+    localStorage.setItem("postName", JSON.stringify(postName))
+ }
 }
