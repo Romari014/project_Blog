56077c000e181e8f4ef5e57827a9ad4c0b76c560
Roman <roma00062@gmail.com>
2022-02-14 21:09:12 +0100

    Created loader before render posts

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import {NavigationComponent} from './components/navigationComponent';
 import {CreateComponent} from './components/createComponent';
 import {FavoriteComponent} from './components/favoriteComponent';
 import {PostsComponent} from './components/postsComponent';
+import {LoaderComponent} from './components/loaderComponent';
 
 new HeaderComponent('header');
 
 const navigation = new NavigationComponent('navigation');
+const loader = new LoaderComponent('loader');
 
 const create = new CreateComponent('create');
 const favorite = new FavoriteComponent('favorite');
-const posts = new PostsComponent('posts');
+const posts = new PostsComponent('posts', {loader}); //loader: loader
 
 navigation.registerTabs([
     {name: 'create', component: create},
